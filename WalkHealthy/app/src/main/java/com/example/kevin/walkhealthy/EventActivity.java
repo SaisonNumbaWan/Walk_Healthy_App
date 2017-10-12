8cecaa187d24ecf9db448ff6311a2853c41213c7
Unknown <daughertyb@etsu.edu>
2017-10-12 02:30:55 -0400

    //Updated Query Results

--- WalkHealthy/app/src/main/java/com/example/kevin/walkhealthy/EventActivity.java
+++ WalkHealthy/app/src/main/java/com/example/kevin/walkhealthy/EventActivity.java
@@ -44,6 +44,8 @@ public class EventActivity extends AppCompatActivity {
         Button searchEventBtn = (Button) findViewById(R.id.btnSearchEvent);
         Button createEventBtn = (Button) findViewById(R.id.btnCreateEvent);
         final TextView eventNameView = (TextView)findViewById(R.id.eventNameTextView);
+        final TextView eventStartView = (TextView) findViewById(R.id.eventStartingLocationTextView);
+        final TextView eventDateView = (TextView)findViewById(R.id.eventDateTextView);
         final EditText editTextLocation = (EditText) findViewById(R.id.editTextLocation);
 
 
@@ -68,7 +70,8 @@ public class EventActivity extends AppCompatActivity {
                                 if(ds.child("EventStartingLocation").getValue().equals(strLocation))
                                 {
                                     eventNameView.setText(ds.child("EventName").getValue().toString());
-                                    eventNameView.setText(ds.getValue().toString());
+                                    eventStartView.setText("Starts At: "+ds.child("EventStartingLocation").getValue().toString());
+                                    eventDateView.setText("On: "+ds.child("EventMonth").getValue().toString()+", "+ds.child("EventDay").getValue().toString());
                                 }
                             }
 
